4c6565cc300659534aa91e60c7b26baab0b86b87
NateTewolde <tewolde.nate@gmail.com>
2022-09-18 21:20:56 -0700

    Add tests to verify that ship is being placed in correct direction

--- tests/gameboard-manager.test.js
+++ tests/gameboard-manager.test.js
@@ -7,22 +7,136 @@ test("10x10 Gameboard created", () => {
   expect(gameboard.length).toBe(10);
 });
 
-test("Place ship at coordinate", () => {
+test("Gameboard.placeShip() works at inital coordinate", () => {
   const myGameboard = Gameboard();
-  myGameboard.placeShip(1, 4, "left", Ship(3));
+  myGameboard.placeShip(4, 1, "left", Ship(3));
   const gameboard = myGameboard.getGameboard();
   expect(gameboard[4][1]).toMatchObject({
     length: 3,
     damaged: [false, false, false],
   });
-  expect(gameboard[4][2]).toMatchObject({
-    length: 3,
-    damaged: [false, false, false],
+
+  gameboard[4][1].hit(0);
+  gameboard[4][1].hit(1);
+  gameboard[4][1].hit(2);
+  expect(gameboard[4][1].isSunk()).toBe(true);
+});
+
+test("Ship.length gets the correct length", () => {
+  const myGameboard = Gameboard();
+  myGameboard.placeShip(4, 1, "left", Ship(4));
+  const gameboard = myGameboard.getGameboard();
+
+  expect(gameboard[4][1].length).toBe(4);
+});
+
+describe("Gameboard.placeShip() places ship in correct direction", () => {
+  test("Places ship left", () => {
+    const myGameboard = Gameboard();
+    myGameboard.placeShip(4, 1, "left", Ship(4));
+    const gameboard = myGameboard.getGameboard();
+    gameboard[4][1].hit(1);
+    expect(gameboard[4][1]).toMatchObject({
+      length: 4,
+      damaged: [false, true, false, false],
+    });
+
+    expect(gameboard[3][1]).toMatchObject({
+      length: 4,
+      damaged: [false, true, false, false],
+    });
+
+    expect(gameboard[2][1]).toMatchObject({
+      length: 4,
+      damaged: [false, true, false, false],
+    });
+
+    expect(gameboard[1][1]).toMatchObject({
+      length: 4,
+      damaged: [false, true, false, false],
+    });
   });
 
-  gameboard[4][2].hit(0);
-  gameboard[4][2].hit(1);
-  gameboard[4][2].hit(2);
-  expect(gameboard[4][2].isSunk()).toBe(true);
-  expect(gameboard[4][1].isSunk()).toBe(true);
+  test("Places ship right", () => {
+    const myGameboard = Gameboard();
+    myGameboard.placeShip(4, 7, "right", Ship(5));
+    const gameboard = myGameboard.getGameboard();
+    gameboard[4][7].hit(2);
+    expect(gameboard[4][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[5][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[6][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[7][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[8][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+  });
+
+  test("Places ship up", () => {
+    const myGameboard = Gameboard();
+    myGameboard.placeShip(4, 7, "up", Ship(5));
+    const gameboard = myGameboard.getGameboard();
+    gameboard[4][7].hit(2);
+    expect(gameboard[4][7]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[4][6]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[4][5]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[4][4]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+
+    expect(gameboard[4][3]).toMatchObject({
+      length: 5,
+      damaged: [false, false, true, false, false],
+    });
+  });
+
+  test("Places ship down", () => {
+    const myGameboard = Gameboard();
+    myGameboard.placeShip(4, 7, "down", Ship(3));
+    const gameboard = myGameboard.getGameboard();
+    gameboard[4][7].hit(0);
+    expect(gameboard[4][7]).toMatchObject({
+      length: 3,
+      damaged: [true, false, false],
+    });
+
+    expect(gameboard[4][8]).toMatchObject({
+      length: 3,
+      damaged: [true, false, false],
+    });
+
+    expect(gameboard[4][9]).toMatchObject({
+      length: 3,
+      damaged: [true, false, false],
+    });
+  });
 });
