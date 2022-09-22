0c9159302d358bf8a8095a3217d3229624634d63
NateTewolde <tewolde.nate@gmail.com>
2022-09-21 21:53:58 -0700

    Fix incorrect call to displayBoard from using the wrong parameter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import Player from "./player-manager";
 import Gameboard from "./gameboard-manager";
 import Ship from "./ship-manager";
 import displayBoard from "./dom-manager";
+import "./styles/style.css";
 
 const humanPlayer = Player(Gameboard());
 const computerPlayer = Player(Gameboard());
+
+humanPlayer.gameboard.placeShip(0, 0, "left", Ship(1));
 
 // places ship at x,y coordinates (1,8), (1,7), (1,6), (1,5), (1,4)
 humanPlayer.gameboard.placeShip(1, 8, "up", Ship(5));
 
 // places ship at x,y coordinates (9,8), (8,8), (7,8), (6,8)
-humanPlayer.gameboard.placeShip(1, 8, "left", Ship(4));
+// humanPlayer.gameboard.placeShip(1, 8, "left", Ship(4));
 
 // places ship at x,y coordinates (4,7), (4,8), (4,9)
 humanPlayer.gameboard.placeShip(4, 7, "down", Ship(3));
@@ -25,7 +28,7 @@ humanPlayer.gameboard.placeShip(0, 4, "right", Ship(1));
 computerPlayer.gameboard.placeShip(1, 8, "up", Ship(5));
 
 // places ship at x,y coordinates (9,8), (8,8), (7,8), (6,8)
-computerPlayer.gameboard.placeShip(1, 8, "left", Ship(4));
+// computerPlayer.gameboard.placeShip(1, 8, "left", Ship(4));
 
 // places ship at x,y coordinates (4,7), (4,8), (4,9)
 computerPlayer.gameboard.placeShip(4, 7, "down", Ship(3));
@@ -39,6 +42,8 @@ computerPlayer.gameboard.placeShip(0, 4, "right", Ship(1));
 // const content = document.getElementById("content");
 const playerSection = document.querySelector(".player-section");
 const computerSection = document.querySelector(".computer-section");
-
-playerSection.appendChild(displayBoard(humanPlayer.gameboard));
-computerSection.appendChild(displayBoard(computerPlayer.gameboard));
+console.log(humanPlayer.gameboard.getGameboard());
+playerSection.appendChild(displayBoard(humanPlayer.gameboard.getGameboard()));
+computerSection.appendChild(
+  displayBoard(computerPlayer.gameboard.getGameboard())
+);
