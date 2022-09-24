b8d6817993e10de8926dcc3cf4e2693bf8df4f34
NateTewolde <tewolde.nate@gmail.com>
2022-09-24 03:13:24 -0700

    Remove setAttack and formatGrid as they fit better in the game-manager module and index module. Add displayWinner to change the game-header to state the winner

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -9,25 +9,6 @@ const clearGame = function removeElementsInGameSections() {
   removeAllChildNodes(document.querySelector(".computer-section"));
 };
 
-const setAttack = function setAttackOnEnemyGameboard(gridId, player1, player2) {
-  const col = gridId.split(",")[0];
-  const row = gridId.split(",")[1];
-  player1.attack(player2, col, row);
-};
-
-const formatGrids = function formatGridsToBeClickedOn(player1, player2) {
-  const grids = document.querySelectorAll(".computer");
-  grids.forEach((grid) =>
-    grid.addEventListener("click", () => {
-      const gridId = grid.getAttribute("data-grid-id");
-      setAttack(gridId, player1, player2);
-      clearGame(player1, player2);
-      // eslint-disable-next-line no-use-before-define
-      displayGame(player1, player2);
-    })
-  );
-};
-
 const displayBoard = function displayPlayerGameBoard(player) {
   const gameboard = player.gameboard.getBoard();
   const gameboardElement = document.createElement("div");
@@ -41,7 +22,9 @@ const displayBoard = function displayPlayerGameBoard(player) {
       const grid = document.createElement("div");
       grid.classList.add("grid");
       grid.setAttribute("data-grid-id", `${i},${j}`);
-      grid.textContent = `${j}`;
+      if (i === 0) {
+        grid.textContent = `${j}`;
+      }
 
       if (gameboard[i][j] != null) {
         grid.classList.add("ship");
@@ -60,14 +43,26 @@ const displayBoard = function displayPlayerGameBoard(player) {
   return gameboardElement;
 };
 
-// const displayGame =
-// eslint-disable-next-line func-style
-function displayGame(player1, player2) {
+const displayGame = function displayTheGame(player1, player2) {
   const playerSection = document.querySelector(".player-section");
   const computerSection = document.querySelector(".computer-section");
   computerSection.appendChild(displayBoard(player2));
   playerSection.appendChild(displayBoard(player1));
-  formatGrids(player1, player2);
-}
+};
 
-export default displayGame;
+const displayWinner = function displayTheGivenPlayerObjAsWinner(player) {
+  const winner = document.createElement("div");
+  winner.classList.add("winner-header");
+
+  if (player.isComputer) {
+    winner.textContent = "Computer wins!";
+  } else {
+    winner.textContent = "You won!";
+  }
+
+  const gameHeader = document.querySelector(".game-header");
+  removeAllChildNodes(gameHeader);
+  gameHeader.appendChild(winner);
+};
+
+export { displayGame, clearGame, displayWinner };
