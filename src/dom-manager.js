6b23ae1562adb11f793c18030e07bb280f314ad8
NateTewolde <tewolde.nate@gmail.com>
2022-09-25 21:57:56 -0700

    Create displayChooseShip() to display and manage the user choosing ships option

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -4,6 +4,10 @@ const removeAllChildNodes = function removeAllChildNodes(parent) {
   }
 };
 
+const clearBoard = function clearUsersPlayerBoard() {
+  removeAllChildNodes(document.querySelector(".player-section"));
+};
+
 const clearGame = function removeElementsInGameSections() {
   removeAllChildNodes(document.querySelector(".player-section"));
   removeAllChildNodes(document.querySelector(".computer-section"));
@@ -49,8 +53,149 @@ const displayBoard = function displayPlayerGameBoard(player) {
 const displayGame = function displayTheGame(player1, player2) {
   const playerSection = document.querySelector(".player-section");
   const computerSection = document.querySelector(".computer-section");
-  computerSection.appendChild(displayBoard(player2));
+
+  const playerHeader = document.createElement("div");
+  playerHeader.classList.add("gameboard-header");
+  playerHeader.textContent = "Your Board";
+
+  const computerHeader = document.createElement("div");
+  computerHeader.classList.add("gameboard-header");
+  computerHeader.textContent = "Enemy's Board";
+
+  playerSection.appendChild(playerHeader);
   playerSection.appendChild(displayBoard(player1));
+
+  if (!player2) {
+    return;
+  }
+  computerSection.appendChild(computerHeader);
+  computerSection.appendChild(displayBoard(player2));
+};
+
+// 'Choose location' then 'Choose direction' with some header on top with arrows.
+// im not gonna even deal with overlapping. maybe i can.
+// put it in computer section
+
+// Psuedo: formatGrid to choose location, a drop down with directions is there,
+//         and a submit button. When submit button is hit it saves location and direction.
+//
+//         displayChooseShip(player) -> calls formatShipChoiceGrid (player, shipTypes) (gridID)
+//                                   -> calls getDirection() which will return
+//                                   -> when a submit button is hit
+//                                   -> calls LocationPromt()
+//                                   -> pops shipTypes. When no more ship types
+//                                   -> It calls chooseComputerShips() and then starts the game.
+//
+const locationPrompt = function promptUserForLocationPlacement(shipType) {
+  const addShipPrompt = document.createElement("div");
+  addShipPrompt.classList.add("add-ship-prompt");
+  addShipPrompt.textContent = `Choose ${shipType[0]} location (length ${shipType[1]}).`;
+
+  return addShipPrompt;
+};
+
+const displayDirectionForm =
+  function createAndDisplayTheDirectionFormForShipPlacement() {
+    const formSection = document.createElement("div");
+    formSection.setAttribute("id", "direction-form-section");
+
+    const directionForm = document.createElement("form");
+    directionForm.setAttribute("action", "");
+    directionForm.setAttribute("method", "post");
+    directionForm.setAttribute("id", "new_direction_form");
+
+    const directionField = document.createElement("div");
+    directionField.classList.add("form-field");
+
+    const directionLabel = document.createElement("label");
+    directionLabel.setAttribute("for", "direction");
+    directionLabel.textContent = "Ship direction?\n";
+    const directionSelect = document.createElement("select");
+    directionSelect.setAttribute("id", "direction");
+    const directionOptionRight = document.createElement("option");
+    directionOptionRight.setAttribute("value", "right");
+    directionOptionRight.setAttribute("selected", "");
+    directionOptionRight.textContent = "Right";
+    const directionOptionLeft = document.createElement("option");
+    directionOptionLeft.setAttribute("value", "left");
+    directionOptionLeft.textContent = "Left";
+    const directionOptionDown = document.createElement("option");
+    directionOptionDown.setAttribute("value", "down");
+    directionOptionDown.textContent = "Down";
+    const directionOptionUp = document.createElement("option");
+    directionOptionUp.setAttribute("value", "up");
+    directionOptionUp.textContent = "Up";
+    directionSelect.appendChild(directionOptionRight);
+    directionSelect.appendChild(directionOptionLeft);
+    directionSelect.appendChild(directionOptionDown);
+    directionSelect.appendChild(directionOptionUp);
+    directionField.appendChild(directionLabel);
+    directionField.appendChild(directionSelect);
+    directionForm.appendChild(directionField);
+
+    const submitBtn = document.createElement("button");
+    submitBtn.classList.add("submit-form-btn");
+    submitBtn.setAttribute("type", "button");
+    submitBtn.textContent = "Submit";
+    directionForm.appendChild(submitBtn);
+    console.log(directionForm);
+    return directionForm;
+  };
+
+const formatShipChoiceGrids = function formatGridsToRunGame(player) {
+  let counter = 0;
+  const grids = document.querySelectorAll(".grid");
+  grids.forEach((grid) =>
+    grid.addEventListener("click", () => {
+      if (counter > 0) {
+        return;
+      }
+      const gridId = grid.getAttribute("data-grid-id");
+      console.log(gridId);
+      grid.classList.add("placement-location");
+      counter++;
+    })
+  );
+};
+
+const shipTypes = [
+  ["Patrol Boat", 2],
+  ["Submarine", 3],
+  ["Destroyer", 3],
+  ["Battleship", 4],
+  ["Carrier", 5],
+];
+
+const formatSubmitBtn = function formatsDirectionSubmitButtonWhenClicked() {
+  const submitFormBtn = document.querySelector(".submit-form-btn");
+  submitFormBtn.addEventListener("click", () => {
+    const location = document.querySelector(".placement-location");
+    const gridId = location.getAttribute("data-grid-id");
+
+    const directionSelected = document.getElementById("direction");
+    const directionChoice =
+      directionSelected.options[directionSelected.selectedIndex].text;
+
+    console.log(gridId + directionChoice + shipTypes[shipTypes.length - 1]);
+    shipTypes.pop();
+  });
+};
+
+const displayChooseShip = function getShipPlacementFromUser(player) {
+  const placeShipContainer = document.createElement("div");
+  placeShipContainer.classList.add("add-ship-container");
+
+  placeShipContainer.appendChild(
+    locationPrompt(shipTypes[shipTypes.length - 1])
+  );
+  displayGame(player);
+  formatShipChoiceGrids(player);
+  placeShipContainer.appendChild(displayDirectionForm());
+
+  const computerSection = document.querySelector(".computer-section");
+  computerSection.appendChild(placeShipContainer);
+
+  formatSubmitBtn();
 };
 
 const displayWinner = function displayTheGivenPlayerObjAsWinner(player) {
@@ -68,4 +213,4 @@ const displayWinner = function displayTheGivenPlayerObjAsWinner(player) {
   gameHeader.appendChild(winner);
 };
 
-export { displayGame, clearGame, displayWinner };
+export { displayGame, clearGame, displayWinner, displayChooseShip };
